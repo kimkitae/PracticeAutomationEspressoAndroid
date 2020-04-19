8ab0158b1eb49dc6c0dce07625c06e3bb6193a4f
KimKitae <daearcdo@kimkitae.me>
2020-04-19 17:05:26 +0900

    초기 파일 업로드

--- practiceAutomationAppiumWithAndroid/app/src/androidTest/java/com/example/practiceautomationappiumwithandroid/ExampleInstrumentedTest.java
+++ practiceAutomationAppiumWithAndroid/app/src/androidTest/java/com/example/practiceautomationappiumwithandroid/ExampleInstrumentedTest.java
@@ -2,26 +2,53 @@ package com.example.practiceautomationappiumwithandroid;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.practiceautomationappiumwithandroid", appContext.getPackageName());
+    }
+
+    @Test
+    public void testCase1() throws Exception{
+
+        Thread.sleep(2000);
+        onView(withId(R.id.textView1)).check(matches(withText("Hello World!")));
+        Thread.sleep(2000);
+        onView(withId(R.id.button1)).perform(click());
+        Thread.sleep(2000);
     }
 }
